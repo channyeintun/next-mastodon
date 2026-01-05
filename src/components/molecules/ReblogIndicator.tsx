364519b510f482b9782e0acc330fd425bf06c334
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2026-01-05 22:08:34 +0700

    fix: update reblog indicator as link to profile

--- src/components/molecules/ReblogIndicator.tsx
+++ src/components/molecules/ReblogIndicator.tsx
@@ -2,7 +2,10 @@
 
 import styled from '@emotion/styled';
 import { Repeat2 } from 'lucide-react';
+import Link from 'next/link';
+import { useQueryClient } from '@tanstack/react-query';
 import { EmojiText } from '@/components/atoms';
+import { prefillAccountCache } from '@/api';
 import type { Account } from '@/types';
 
 interface ReblogIndicatorProps {
@@ -14,16 +17,23 @@ interface ReblogIndicatorProps {
  * showing who boosted the post.
  */
 export function ReblogIndicator({ account }: ReblogIndicatorProps) {
+    const queryClient = useQueryClient();
+
+    // Pre-populate account cache before navigation
+    const handleProfileClick = () => {
+        prefillAccountCache(queryClient, account);
+    };
+
     return (
         <Container>
             <Icon size={14} />
             <span>
-                <strong>
+                <ProfileLink href={`/@${account.acct}`} onClick={handleProfileClick}>
                     <EmojiText
                         text={account.display_name || account.username}
                         emojis={account.emojis}
                     />
-                </strong> boosted
+                </ProfileLink> boosted
             </span>
         </Container>
     );
@@ -40,4 +50,14 @@ const Container = styled.div`
 
 const Icon = styled(Repeat2)`
   margin-left: var(--size-6);
+`;
+
+const ProfileLink = styled(Link)`
+  font-weight: var(--font-weight-6);
+  color: inherit;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `;
